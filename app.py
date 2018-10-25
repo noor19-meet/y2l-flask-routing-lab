aeae417dbc8f958cacee0b64390c9a100bddeeb6
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:45:10 +0300

    3 day

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import *
+from flask import render_template, Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+@app.route('/shop')
+def shop_page():
+    return render_template("shop.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
